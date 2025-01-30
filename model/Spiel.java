22530d31d34986d5b33e7b092a39f81b4caa233b
antmyd <antonmydlach@gmail.com>
2025-01-30 18:13:59 +0100

    Spiel erweitert

--- model/Spiel.java
+++ model/Spiel.java
@@ -1,5 +1,6 @@
 package model;
 
+import game.Spieler;
 import java.util.Scanner;
 
 
@@ -19,19 +20,26 @@ public class Spiel {
         System.out.println();
         System.out.println("Der böse Kobold Winky hat dir gerade eben noch deine ganzen 14 Goldmünzen gestohlen.");
         System.out.println("und ist damit in die Grube geflüchtet. Du hast keine Wahl außer ihm zu folgen, wenn du dein Gold zurückhaben willst.");
+        System.out.println();
         System.out.println("Als Spieler hast du gewisse Eigenschaften, die dir bei deiner Aufgabe, dein Gold zurückzubekommen helfen können.");
+        System.out.println();
         System.out.println("Willst du diese einsehen? Ja oder nein?");
 
         String choice = sc.nextLine();
 
         if (choice.equalsIgnoreCase("ja")) {
-            print();
+             Spieler spieler = new Spieler(name, 50, 0, "Faust");  // Beispiel Initialisierung
+                spieler.print(); 
 
         } else if (choice.equalsIgnoreCase("nein")) {
             System.out.println("Dann kann dein Abenteuer losgehen!");
             abenteuer();
+        
+        } else { 
+            System.out.println("Unglütige Eingabe.");
+            System.exit(0);
         }
-        }
+    }
     
         public static void abenteuer() {
         System.out.println("Du betrittst den Eingang der Grube");
