0d1ba7cbea60d09c659ddb7dc5f5bba2b9677084
antmyd <antonmydlach@gmail.com>
2025-02-15 16:22:13 +0100

    galgenmännchen fertig

--- game/Galgenmännchen.java
+++ game/Galgenmännchen.java
@@ -1,10 +1,11 @@
 package game;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Galgenmännchen {
 
-    private static int Versuche = 5;
+    private static int Versuche = 10;
 
     public Galgenmännchen(int Versuche) {
         this.Versuche = Versuche;
@@ -13,140 +14,132 @@ public class Galgenmännchen {
     }
 
     public static int getVersuche() {
-            return Versuche;
-         }
+        return Versuche;
+    }
     
          
         
         
-         public static void main(String[] args) {
-            Random rand = new Random();
-            Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        
+        Galgenmännchen spiel = new Galgenmännchen(Versuche);
+        
+        String[] Wörter = {"goldmünzen", "fackel", "winky", "spinne", "riese", "schwert", "heiltrank", "dunkelheit", "schloss", "larry", "wald"};
+        int zufallsgenerator = rand.nextInt(0, 11);
+        String Wort = "riese";//Wörter[zufallsgenerator];
+        ArrayList <String> errateneBuchstaben  = new ArrayList<>();
+
+
+
+
+
+        
+
+
+
+        
             
-            Galgenmännchen spiel = new Galgenmännchen(Versuche);
+        
+
+        System.out.println("Willkommen zu Galgenmännchen!");
+
+        System.out.println("Das gesuchte Wort: ");
+
+        for(int i = 0; i < Wort.length(); i++) {
+            System.out.print("_ ");
+        }
+        System.out.println();
+        System.out.println("Du hast 5 Versuche, das Wort zu erraten.");
+        System.out.println();
+
+        
+        
+        while (Versuche > 0) {
+
+            System.out.print("                   Willst du das Wort direkt erraten? Ja/Nein?");
             
-            String[] Wörter = {"Goldmünzen", "Fackel", "Winky", "Spinne", "Riese", "Schwert", "Heiltrank", "Dunkelheit", "Schloss", "Larry", "Wald"};
-            int zufallsgenerator = rand.nextInt(0, 11);
-            
-            String Wort = Wörter[zufallsgenerator];
+
+            String auswahl = sc.nextLine();
+
+            if (auswahl.equalsIgnoreCase("ja")) {
+                System.out.println("Gebe das Wort ein: ");
+                String rateWort = sc.nextLine();
+
+                if (rateWort.equalsIgnoreCase(Wort)) {
+                    System.out.println("Richtig! Du hast gewonnen.");
+                    return;
+
+                } else {
+                    System.out.println("Falsch! Du hast 3 Versuche verloren!");
+                    Versuche = spiel.getVersuche() - 3;
+                }
     
-            
-               
-            
-    
-            System.out.println("Willkommen zu Galgenmännchen!");
-    
-            System.out.println("Das gesuchte Wort: ");
-    
-            for(int i = 0; i < Wort.length(); i++) {
-                System.out.print("_ ");
-            }
-            System.out.println();
-            System.out.println("Du hast 5 Versuche, das Wort zu erraten.");
-    
-            
-            
-            while (Versuche > 0) {
-    
-                wortDirektErraten(spiel, sc, Wort, Versuche);
-                
+            } else if (auswahl.equalsIgnoreCase("nein")) {
+                //Spieler möchte Buchstabe für Buchstabe
                 System.out.println();
                 System.out.print("Gebe einen Buchstaben ein: ");
                 System.out.print("                    Deine Versuche: " + getVersuche());
                 System.out.println();
                 String Buchstabe = sc.nextLine();
-            
-           
-    
-            
+                Buchstabe = Buchstabe.toLowerCase();
 
-            if (Wort.contains(Buchstabe)) {
-                System.out.println("Richtig!");
-                System.out.println();
-                Versuche = getVersuche();
-                System.out.println();
-                
-                
-                for(int i = 0; i < Wort.length(); i++) {
-                    if (Buchstabe.equalsIgnoreCase(Character.toString(Wort.charAt(i)))) {
-                        System.out.print(Buchstabe);
-                    } else {
-                        System.out.print("_ ");
+                if (Wort.contains(Buchstabe)) {
+                    System.out.println("Richtig!");
+                    System.out.println();
+
+                    if (!errateneBuchstaben.contains(Buchstabe)) {
+                        errateneBuchstaben.add(Buchstabe);
                     }
                     
+                    
+                    zeigFortschritt(Wort, errateneBuchstaben);
+
+                    
+                } else {
+                    System.out.println("Falsch!");
+                    System.out.println(); 
+                    Versuche = getVersuche() - 1;
+                    zeigFortschritt(Wort, errateneBuchstaben);
+    
+    
                 }
-
-               
-
-                
-                
-                
-                
-            } else {
-                System.out.println("Falsch!");
-                System.out.println();
-                Versuche = getVersuche() - 1;
-
-                
-                
-                
-                
-        
-        }
-        
-       
-
-        
-        
-    }
     
     
-    System.out.println();
-    System.out.println("Verloren!");
-
-            
-       
-
+            }
         
-             
-
-            
-                
-                
-            
         
+    
+        }       
+            
+
+
+        System.out.println();
+        System.out.println("Verloren!");
+        System.out.println("Das gesuchte Wort war " + Wort);
+
+
+    
     }
-               
-    public static void wortDirektErraten(Galgenmännchen spiel, Scanner sc, String Wort, int Versuche) {
 
-       
+    public static void zeigFortschritt(String Wort, ArrayList<String> errateneBuchstaben) {
+        for(int i = 0; i < Wort.length(); i++) {
+            boolean found = false;
+            for(int j = 0; j < errateneBuchstaben.size(); j++) {
+                String aktuellerBuchstabe = errateneBuchstaben.get(j);
 
-        
-        System.out.println("Willst du das Wort direkt erraten? Ja/Nein?");
+                if (aktuellerBuchstabe.equalsIgnoreCase(Character.toString(Wort.charAt(i)))) {
+                    System.out.print(" " + aktuellerBuchstabe);
+                    found = true;
+                }
+    
+            }
 
-        String auswahl = sc.nextLine();
-
-        if (auswahl.equalsIgnoreCase("ja")) {
-           System.out.println("Gebe das Wort ein: ");
-           String rateWort = sc.nextLine();
-
-           if (rateWort.equalsIgnoreCase(Wort)) {
-               System.out.println("Richtig! Du hast gewonnen.");
-               
-               System.exit(0);
-               
-
-           } else {
-               System.out.println("Falsch! Du hast 3 Versuche verloren!");
-               Versuche = spiel.getVersuche() - 3;
-           }
-        } else if (auswahl.equalsIgnoreCase("nein")) {
-                
+            if (!found) {
+                System.out.print(" _");
+            }
         }
-         
-     }
+    }
 
-        
-
-       
 }
